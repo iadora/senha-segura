af129d8ad08e09677fbced54d4adae88095f352d
isadora <142797808+iadora@users.noreply.github.com>
2025-07-03 08:34:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const numeroSenha = document.querySelector(".parametro-senha__texto")
 let tamanhoSenha = 12
 numeroSenha.textContent = tamanhoSenha
 const botoes = document.querySelectorAll(".parametro-senha__botao")
-const checkbox = document.querySelectorAll("parametro-senha_botao")
+const checkbox = document.querySelectorAll('.checkbox')
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ'
-const letrasMinusculas ='abcdefghijklmnopqrstuvxywz'
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz'
 const numeros = '0123456789'
-const simbolos = '!@%*?'
+const simbolos =  '!@%*?'
+
 function diminuiTamanho(){
     if(tamanhoSenha>1){
         tamanhoSenha --
@@ -31,13 +32,43 @@ botoes[1].onclick = aumentaTamanho
 
 const campoSenha = document.querySelector('#campo-senha')
 
+for(i=0;i<checkbox.length;i++)(
+    checkbox[i].onclick=geraSenha
+)
 function geraSenha(){
-    let senha = ''
+    let alfabeto=''
+    if(checkbox[0].checked){
+        alfabeto=alfabeto + letrasMaiusculas
+    }
+    if(checkbox[1].checked){
+        alfabeto=alfabeto + letrasMinusculas
+    }
+     if(checkbox[2].checked){
+        alfabeto=alfabeto + numeros
+    }
+   
+     if(checkbox[3].checked){
+        alfabeto=alfabeto + simbolos
+    }
+let senha= ""
     for(let i =0; i< tamanhoSenha; i++){
 
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length
+        let numeroAleatorio = Math.random()*alfabeto.length
     numeroAleatorio = Math.floor(numeroAleatorio);
-    senha = senha + letrasMaiusculas[numeroAleatorio]
+    senha = senha + alfabeto[numeroAleatorio]
     }
     campoSenha.value = senha 
+    classificaSenha()
+}
+const forcaSenha = document.querySelector('.forca')
+function classificaSenha(){
+    console.log('oi')
+    forcaSenha.classList.remove('fraca', 'media', 'forte')
+    if(tamanhoSenha>11){
+    forcaSenha.classList.add('forte')}
+else if (tamanhoSenha>5 && tamanhoSenha<12){
+    forcaSenha.classList.add('media')
+}else if (tamanhoSenha<=5){
+    forcaSenha.classList.add('fraca')
+}
 }
